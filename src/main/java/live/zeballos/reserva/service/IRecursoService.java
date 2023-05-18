224a7f0fe1da1ed6977f464ee34ffe7ec7fb5a4f
LeooZeballos <zeballosleonel3@gmail.com>
2023-05-17 23:17:29 -0300

    unused imports de lists

--- src/main/java/live/zeballos/reserva/service/IRecursoService.java
+++ src/main/java/live/zeballos/reserva/service/IRecursoService.java
@@ -4,8 +4,6 @@ import live.zeballos.reserva.model.Recurso;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface IRecursoService extends IService<Recurso> {
     Page<Recurso> getAll(Pageable page, String nombre);
 }
